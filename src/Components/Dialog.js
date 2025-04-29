a5d8e5f9a07af1918e46658b8e5a55d13faef0a6
Ekta Joshi <ekajoshi95@gmail.com>
2025-04-29 19:24:20 +0530

    commit 3

--- src/Components/Dialog.js
+++ src/Components/Dialog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Dialog,
   DialogTitle,
@@ -12,8 +12,12 @@ import {
   Button,
   styled,
 } from "@mui/material";
-import { Circle, RectangleHorizontal, RectangleHorizontalIcon, Square, StarIcon } from "lucide-react";
-import { Star } from "react-konva";
+import {
+  Circle,
+  RectangleHorizontalIcon,
+  Square,
+  StarIcon,
+} from "lucide-react";
 
 const categories = [
   "Basic",
@@ -31,22 +35,44 @@ const categories = [
   "Activities",
 ];
 
-
-
 const ShapeDialog = ({ open, onClose, onSelectShape }) => {
+  const [search, setSearch] = useState("");
 
-    
   return (
-    <Dialog open={open} onClose={onClose} fullWidth maxWidth="md">
+    <Dialog open={open} onClose={onClose} maxWidth="sm">
+      <Box
+        display="flex"
+        margin="20px"
+        justifyContent="center"
+        alignItems="center"
+        gap={1}
+        mb={2}
+      >
+        <Button
+          sx={{
+            marginRight: "0px",
+          }}
+          variant="contained"
+        >
+          🔎
+        </Button>
+
+        <TextField
+          size="small"
+          fullWidth
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </Box>
       <Box display="flex" height="500px">
-        <Box width="200px" bgcolor="#f5f5f5" p={1} overflow="auto">
-          <TextField
-            size="small"
-            placeholder="Search"
-            fullWidth
-            variant="outlined"
-            sx={{ mb: 1 }}
-          />
+        <Box
+          width="200px"
+          bgcolor="#f5f5f5"
+          p={1}
+          overflow="auto"
+          sx={{ borderRight: "1px solid #ccc" }}
+        >
           <List dense>
             {categories.map((cat) => (
               <ListItem button key={cat}>
@@ -67,24 +93,27 @@ const ShapeDialog = ({ open, onClose, onSelectShape }) => {
                 variant="text"
                 onClick={() => onSelectShape("rect")}
               >
-                <RectangleHorizontalIcon/>
+                <RectangleHorizontalIcon />
               </Button>
               <Button
-              sx={{
+                sx={{
                   margin: "4px",
                   cursor: "pointer",
                 }}
                 variant="text"
                 onClick={() => onSelectShape("circle")}
               >
-                <Circle/>
+                <Circle />
               </Button>
               <Button
-              sx={{
+                sx={{
                   margin: "4px",
                   cursor: "pointer",
-                }} variant="text" onClick={() => onSelectShape("star")}>
-                <StarIcon/>
+                }}
+                variant="text"
+                onClick={() => onSelectShape("star")}
+              >
+                <StarIcon />
               </Button>
             </Grid>
             {/* ))} */}
